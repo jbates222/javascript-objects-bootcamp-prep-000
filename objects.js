13d2916f7b9d968f8b67f19276960fe20ddee20d
Jackson Bates <jkbates222@gmail.com>
2018-09-19 00:17:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { 'artist': 'song' };
-function updatePlaylist(playlist, artist, song) {
+function updatePlaylist( playlist, artist, song) {
   return Object.assign({}, artist, song)
 }
