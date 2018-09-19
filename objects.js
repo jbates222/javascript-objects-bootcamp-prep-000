01ac139c57b70e8b1d535c8400100a8c667689d5
Jackson Bates <jkbates222@gmail.com>
2018-09-19 00:21:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {};
 var key = "artist";
-playlist[key] = 
-function updatePlaylist( playlist, artist, song) {
-  return Object.assign({}, artist, song)
+
+function updatePlaylist( playlist, key, value) {
+  return Object.assign(playlist, { [key]: value })
 }
